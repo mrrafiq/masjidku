b23a951d5c48920250bda2b9f15f16d59588582c
Igumanof <61772074+igumanof22@users.noreply.github.com>
2021-04-21 13:21:46 +0700

    Kurang username

--- app/src/main/java/com/masjidtrpl/masjidku/SignUpActivity.java
+++ app/src/main/java/com/masjidtrpl/masjidku/SignUpActivity.java
@@ -2,15 +2,21 @@ package com.masjidtrpl.masjidku;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,7 +24,8 @@ public class SignUpActivity extends AppCompatActivity {
     EditText user,email,pass,pass1;
     Button daftar;
     DatabaseReference reference;
-    ProgressBar progressBar;
+    FirebaseAuth auth;
+    String getEmail, getPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,25 +39,51 @@ public class SignUpActivity extends AppCompatActivity {
 
 
         reference = FirebaseDatabase.getInstance().getReference();
+        auth = FirebaseAuth.getInstance();
 
+        pass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        pass1.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
         daftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isEmpty(user.getText().toString()) || isEmpty(email.getText().toString()) || isEmpty(pass.getText().toString()) || isEmpty(pass1.getText().toString())) {
+                if (isEmpty(email.getText().toString()) || isEmpty(pass.getText().toString()) || isEmpty(pass1.getText().toString())) {
                     Toast.makeText(SignUpActivity.this, "Data tidak boleh kosong!", Toast.LENGTH_SHORT).show();
                 } else {
-                    reference.child("user").child("akun").push()
-                            .setValue(new ModelSignUp(user.getText().toString(), email.getText().toString(), pass.getText().toString(), pass1.getText().toString()))
-                            .addOnSuccessListener(SignUpActivity.this, new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Toast.makeText(getApplicationContext(), "Data Tersimpan!", Toast.LENGTH_SHORT).show();
-                                }
-                            });
+                    if (pass.equals(pass1)){
+                        cekDataUser();
+                    } else{
+                        Toast.makeText(SignUpActivity.this, "Password Tidak boleh berbeda", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
+    }
+
+    private void cekDataUser() {
+        getEmail = email.getText().toString();
+        getPass = pass.getText().toString();
+
+        if (getPass.length() < 8){
+            Toast.makeText(this, "Panjang password kurang dari 8 karakter", Toast.LENGTH_SHORT).show();
+        } else{
+            createUserAccount();
+        }
+    }
+
+    private void createUserAccount() {
+        auth.createUserWithEmailAndPassword(getEmail, getPass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this, "Sign Up Berhasil", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else{
+                            Toast.makeText(SignUpActivity.this, "Terjadi kesalahan!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private boolean isEmpty(String s){
