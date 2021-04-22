22d8ef0002ac998175f98caf100ddd764a5efb6b
Igumanof <61772074+igumanof22@users.noreply.github.com>
2021-04-22 11:23:50 +0700

    Manifest dan SDK.dir

--- app/src/main/java/com/masjidtrpl/masjidku/ModelSignUp.java
+++ app/src/main/java/com/masjidtrpl/masjidku/ModelSignUp.java
@@ -1,16 +1,14 @@
 package com.masjidtrpl.masjidku;
 
 public class ModelSignUp {
-    private String user, pass, pass1, email;
+    private String user;
 
     public ModelSignUp() {
     }
 
-    public ModelSignUp(String user, String email, String pass, String pass1) {
+    public ModelSignUp(String user) {
         this.user = user;
-        this.pass = pass;
-        this.pass1 = pass1;
-        this.email = email;
+
     }
 
     public String getUser() {
@@ -19,29 +17,5 @@ public class ModelSignUp {
 
     public void setUser(String user) {
         this.user = user;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
-    }
-
-    public String getPass1() {
-        return pass1;
-    }
-
-    public void setPass1(String pass1) {
-        this.pass1 = pass1;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
